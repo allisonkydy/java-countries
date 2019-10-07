fd4a1ddd8ca29e6cc59c9ecb80a4ae3663c23d27
allisonkydy <allisnelly@gmail.com>
2019-10-07 14:13:08 -0500

    create new project using spring boot

---
+++ countrysearch/src/main/java/com/lambda/countrysearch/CountrySearchApplication.java
@@ -0,0 +1,15 @@
+package com.lambda.countrysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrySearchApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(CountrySearchApplication.class, args);
+  }
+
+}
